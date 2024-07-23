f04730c75a6d021c46b3fa59e9ffe50667599b49
LeandroSilva <leandro.silva@virtual-identity.com>
2024-07-23 12:01:56 +0200

    CARITAS-243 - remove forwardMessage api code

--- src/main/java/de/caritas/cob/messageservice/api/controller/MessageController.java
+++ src/main/java/de/caritas/cob/messageservice/api/controller/MessageController.java
@@ -5,13 +5,11 @@ import static java.util.Objects.nonNull;
 
 import de.caritas.cob.messageservice.Messenger;
 import de.caritas.cob.messageservice.api.exception.BadRequestException;
-import de.caritas.cob.messageservice.api.helper.JSONHelper;
 import de.caritas.cob.messageservice.api.model.AliasArgs;
 import de.caritas.cob.messageservice.api.model.AliasMessageDTO;
 import de.caritas.cob.messageservice.api.model.AliasOnlyMessageDTO;
 import de.caritas.cob.messageservice.api.model.ChatMessage;
 import de.caritas.cob.messageservice.api.model.DraftMessageDTO;
-import de.caritas.cob.messageservice.api.model.ForwardMessageDTO;
 import de.caritas.cob.messageservice.api.model.MasterKeyDTO;
 import de.caritas.cob.messageservice.api.model.MessageDTO;
 import de.caritas.cob.messageservice.api.model.MessageResponseDTO;
@@ -118,38 +116,6 @@ public class MessageController implements MessagesApi {
         .type(message.getT()).build();
 
     var response = messenger.postGroupMessage(groupMessage);
-
-    return new ResponseEntity<>(response, HttpStatus.CREATED);
-  }
-
-  /**
-   * Forwards/posts a message in the specified Rocket.Chat group and sets the values from the body
-   * object in the alias object of the Rocket.Chat message.
-   *
-   * @param rcToken           (required) Rocket.Chat token of the user
-   * @param rcUserId          (required) Rocket.Chat user ID
-   * @param rcGroupId         (required) Rocket.Chat group ID
-   * @param forwardMessageDTO (required) {@link ForwardMessageDTO}
-   * @return {@link ResponseEntity} with the {@link HttpStatus}
-   */
-  @Override
-  public ResponseEntity<MessageResponseDTO> forwardMessage(@RequestHeader String rcToken,
-      @RequestHeader String rcUserId, @RequestHeader String rcGroupId,
-      @Valid @RequestBody ForwardMessageDTO forwardMessageDTO) {
-
-    Optional<String> alias =
-        JSONHelper.convertAliasMessageDTOToString(
-            new AliasMessageDTO().forwardMessageDTO(forwardMessageDTO));
-
-    if (alias.isEmpty()) {
-      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
-
-    var forwardMessage = ChatMessage.builder().rcToken(rcToken).rcUserId(rcUserId)
-        .rcGroupId(rcGroupId).text(forwardMessageDTO.getMessage())
-        .type(forwardMessageDTO.getT()).alias(alias.get())
-        .build();
-    var response = messenger.postFeedbackGroupMessage(forwardMessage);
 
     return new ResponseEntity<>(response, HttpStatus.CREATED);
   }
